f4e5931db5a64c091e5210ccb2277a21fdcb945e
frankkwabenaaboagye <frank.aboagye@amalitech.com>
2024-09-08 16:36:24 +0000

    update to default customer service

--- letscode/src/main/java/frankaboagye/letscode/LetscodeApplication.java
+++ letscode/src/main/java/frankaboagye/letscode/LetscodeApplication.java
@@ -3,9 +3,11 @@ package frankaboagye.letscode;
 import lombok.extern.slf4j.Slf4j;
 import org.postgresql.Driver;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -47,6 +49,8 @@ class DefaultCustomerService {
 
     private final JdbcTemplate jdbcTemplate;
 
+    private final RowMapper<Customer> customerRowMapper = (rs, rowNum) -> new Customer(rs.getInt("id"), rs.getString("name"));
+
     DefaultCustomerService(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -56,33 +60,7 @@ class DefaultCustomerService {
 
     Collection<Customer> allCustomers() throws Exception {
 
-        var listOfCustomers = new ArrayList<Customer>();
-
-        try {
-            try (var connection = this.dataSource.getConnection()) {
-
-                try (var stmt = connection.createStatement()) {
-
-                    try (var resultSet = stmt.executeQuery("select * from customers");) {
-
-                        while (resultSet.next()) {
-                            var name = resultSet.getString("name");
-                            var id = resultSet.getInt("id");
-                            listOfCustomers.add(new Customer(id, name));
-
-                        }
-
-                    }
-
-                }
-            }
-        } catch (Exception e) {
-            log.error("Something went wrong - figure out", e);
-
-        }
-
-        return listOfCustomers;
-
+        return  this.jdbcTemplate.query("select * from customers", this.customerRowMapper);
 
     }
 }
